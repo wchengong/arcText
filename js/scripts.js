3e525940c62996c7749ec96b94a49ebf51f5b6eb
wchengong <wchengong@vcd-ch204-03.eastern.ewu.edu>
2016-10-20 14:40:26 -0700

    version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 // My Scripts
+//console.log(tmp)
+
+$('h1').arctext({
+    rotate:true
+});
+
+//radius	: 0, 	
+// the minimum value allowed is 
+// half of the word length. 
+// if set to -1, the word will be straight.
+
+//dir		: 1,	
+// 1: curve is down, 
+// -1: curve is up
+
+//rotate	: true,	
+// if true each letter should be rotated.
+
+//fitText	: false 
+// if you want to try out the 
+// fitText plugin (http://fittextjs.com/) 
+// set this to true. 
+// Don't forget, the wrapper should be fluid.
+
